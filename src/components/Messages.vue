fc4762db69fcedac5b9d89ff64fa484dafacf12c
Kudae Sithu <kudaesithu2@gmail.com>
2024-03-15 15:38:04 +0630

    Added Two-sided chat interface

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -3,28 +3,33 @@
         
         <div class="messages">
         
-            <div class="single-message" v-for="message in FormatMessage" :key="message.id" >
+            <div  class="single-message" v-for="message in FormatMessage" :key="message.id" >
                 
-                <span class="user" >{{ message.user }}</span>
-                <div class="flex space-x-2 items-center pt-1" >
-                    <span class="message" >{{ message.message }}</span>
-                    <span class="time">{{message.created_at}} ago </span>
+                
+                <span v-if="currentUser.displayName != message.user" class="user" >{{ message.user }}</span> <!-- username -->
+                
+                <div :class="{'authUser-msg': currentUser.displayName == message.user}" class="flex space-x-2 items-center pt-1" >  
+                    <span class="message" >{{ message.message }}</span>                  
+                    <span class="time">{{message.created_at}} ago </span>                    
                 </div>
             
-            </div>
-
-    </div>
+            </div>            
+        
+        </div>
+    
     </div>
 </template>
 
 <script>
-    import { db } from '@/firebase/config';
+    import { auth, db } from '@/firebase/config';
     import { computed, ref } from 'vue'
     import { formatDistanceToNow } from "date-fns";
+
 
     export default {
         setup(){
             
+            let currentUser = ref(auth.currentUser)
             let messages = ref([])
             
             // fetch data from firebase
@@ -32,7 +37,7 @@
                 let results = [];
                 snap.docs.forEach( (doc)=>{
                     let document = {...doc.data(),id:doc.id}
-                    if(doc.data().created_at){
+                    if(doc.data().created_at ){
                         results.push(document)
                     }
                 } ) 
@@ -44,11 +49,12 @@
             let FormatMessage = computed( ()=>{
                 return messages.value.map( (msg)=>{
                     let formatTime = formatDistanceToNow(msg.created_at.toDate())
-                    return {...msg, created_at: formatTime}
+                    return {...msg, created_at: formatTime}   
+                               
                 } )
             } )
 
-            return {messages, FormatMessage}
+            return {messages, FormatMessage, currentUser}
         }
     }
 </script>
